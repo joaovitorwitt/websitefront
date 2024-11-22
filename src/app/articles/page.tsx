5de35a558b2bebc65a86c3f580b81cfac38b2665
João Vitor Witt <joaovitorwitt2004@gmail.com>
2024-11-21 21:20:36 -0300

    new dev

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -7,7 +7,6 @@ import Link from "next/link";
 import LoadingComponent from "../components/LoadingComponent";
 import { useEffect, useState } from "react";
 import Image from "next/image";
-import { formatArticleDate, formatTitleForURL } from "@/app/utils";
 import "../assets/css/pages/articles/articles.modules.css";
 import "../assets/css/globals.css";
 import RoundButton from "../components/RoundButton";
@@ -17,11 +16,12 @@ import RoundButton from "../components/RoundButton";
 //////////////////////////////////////////////////////
 interface Article {
   id: number;
-  title: string;
-  description: string;
-  content: string;
-  publish_date: string;
-  thumbnail: string;
+  Title: string;
+  Description: string;
+  Content: string;
+  date: string;
+  image_url: string;
+  url_title: string;
 }
 
 //////////////////////////////////////////////////////
@@ -34,11 +34,11 @@ export default function Articles() {
   useEffect(() => {
     const fetchArticles = async () => {
       try {
-        const response = await fetch(
-          `${process.env.local}/api/v1/get/articles/`
-        );
+        // const response = await fetch(`${process.env.local}/get/articles/`);
+        const response = await fetch(`http://127.0.0.1:5000/get/articles`);
         const result = await response.json();
-        setArticles(result.articles);
+        console.log(result);
+        setArticles(result);
       } catch (error) {
         console.error("Error fetching articles: ", error);
       } finally {
@@ -56,45 +56,37 @@ export default function Articles() {
           <LoadingComponent />
         ) : Array.isArray(articles) && articles.length > 0 ? (
           <div className="posts-wrapper">
-            {articles
-              .sort((a, b) => {
-                const dateA = new Date(a.publish_date).getTime();
-                const dateB = new Date(b.publish_date).getTime();
-                return dateB - dateA;
-              })
-              .map((article) => (
-                <Link
-                  key={article.id}
-                  className="article"
-                  href={`/articles/${formatTitleForURL(article.title)}`}
-                >
-                  <div className="article-wrapper">
-                    <div className="posts-article-image-wrapper">
-                      <Image
-                        src={article?.thumbnail}
-                        key={article.id}
-                        alt="article image"
-                        width={1920}
-                        height={1080}
-                        className="article-image"
-                        loading="lazy"
-                      />
-                    </div>
-
-                    <div className="article-data-container">
-                      <div className="article-data">
-                        <span>{formatArticleDate(article.publish_date)}</span>
-                        <span className="article-data-spacer"></span>
-                      </div>
-
-                      <h3 className="article-title">{article.title}</h3>
-                      <p className="article-description">
-                        {article.description}
-                      </p>
-                    </div>
+            {articles.map((article) => (
+              <Link
+                key={article.id}
+                className="article"
+                href={`/articles/${article.url_title}`}
+              >
+                <div className="article-wrapper">
+                  <div className="posts-article-image-wrapper">
+                    <Image
+                      src={article?.image_url}
+                      key={article.id}
+                      alt="article image"
+                      width={1920}
+                      height={1080}
+                      className="article-image"
+                      loading="lazy"
+                    />
                   </div>
-                </Link>
-              ))}
+
+                  <div className="article-data-container">
+                    <div className="article-data">
+                      <span>{article.date}</span>
+                      <span className="article-data-spacer"></span>
+                    </div>
+
+                    <h3 className="article-title">{article.Title}</h3>
+                    <p className="article-description">{article.Description}</p>
+                  </div>
+                </div>
+              </Link>
+            ))}
           </div>
         ) : (
           <p style={{ textAlign: "center" }}>No articles found.</p>
